dba56b162dddd25ace10631c1afd7eb1f1cea8f8
Yana <yanaafanaseva7@gmail.com>
2022-02-15 13:49:06 +0300

    Second commit

--- src/first.java
+++ src/first.java
@@ -1,5 +1,6 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("Ya tebya lublu");
+        System.out.println("Kak dela");
+        System.out.println("привет");
     }
 }
